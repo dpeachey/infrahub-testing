1db52a63aa22e792fe0651efc200177f0c8250e2
Dan Peachey <dan@netdevs.eu>
2024-10-10 14:29:42 +0000

    Update transform

--- device_transform.py
+++ device_transform.py
@@ -15,10 +15,18 @@ class DeviceTransform(InfrahubTransform):
         site_name = site["name"]["value"]
         site_vlan_ids = [vlan["node"]["vlan_id"]["value"] for vlan in site["vlans"]["edges"]]
 
-        return {
-            "device_name": device_name,
-            "device_description": device_description,
-            "device_type": device_type,
-            "site_name": site_name,
-            "site_vlan_ids": site_vlan_ids
-        }
+        # return {
+        #     "device_name": device_name,
+        #     "device_description": device_description,
+        #     "device_type": device_type,
+        #     "site_name": site_name,
+        #     "site_vlan_ids": site_vlan_ids
+        # }
+
+        return (
+            f"Device: {device_name}\n"
+            f"Description: {device_description}\n"
+            f"Type: {device_type}\n"
+            f"Site: {site_name}\n"
+            f"VLAN IDs: {','.join(str(vlan_id) for vlan_id in site_vlan_ids)}"
+        )
