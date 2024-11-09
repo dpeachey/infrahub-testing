addd8c278e8c0894be894dd484b4acc736b82722
Dan Peachey <dan@netdevs.eu>
2024-11-09 19:48:59 +0000

    Update check

--- checks/loopback_check.py
+++ checks/loopback_check.py
@@ -10,14 +10,16 @@ class LoopbackCheck(InfrahubCheck):
     query: str = "device_loopback_query"
 
     def validate(self, data: dict[str, Any]):
-        for device in data["InfraDevice"]["edges"]:
-            for interface in device["node"]["interfaces"]["edges"]:
-                for ip in interface["node"]["ip_addresses"]["edges"]:
-                    value = ip["node"]["address"].get("value")
-                    device = device["node"]["name"]["value"]
-                    if value and not IS_SLASH_32.match(value):
-                        self.log_error(
-                            message=f"Loopback IP {value} on device {device} must be a /32: {value}",
-                            object_id=value,
-                            object_type="InfraInterfaceL3",
-                        )
+        device = data["InfraDevice"]["edges"][0]
+
+        for interface in device["node"]["interfaces"]["edges"]:
+            for ip in interface["node"]["ip_addresses"]["edges"]:
+                value = ip["node"]["address"].get("value")
+                device = device["node"]["name"]["value"]
+
+                if value and not IS_SLASH_32.match(value):
+                    self.log_error(
+                        message=f"Loopback IP {value} on device {device} must be a /32: {value}",
+                        object_id=value,
+                        object_type="InfraInterfaceL3",
+                    )
